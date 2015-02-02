7992a382cf8f1e3ff5b021bf799b60a19d710b37
NieYong <aofengblog@163.com>
2015-02-02 20:28:49 +0800

    fix bug:import error

--- src/cn/aofeng/demo/controlflow/IfStatement.py
+++ src/cn/aofeng/demo/controlflow/IfStatement.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 # 条件控制语句：if elif else
 
-from cn.aofeng.demo import Guess
+from cn.aofeng.demo.controlflow import Guess
 
 Guess.guess(9)
 print("Done!")
--- src/cn/aofeng/demo/controlflow/WhileStatement.py
+++ src/cn/aofeng/demo/controlflow/WhileStatement.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 # 循环控制语句：while
 
-from cn.aofeng.demo import Guess
+from cn.aofeng.demo.controlflow import Guess
 
 running = True
 while running:
