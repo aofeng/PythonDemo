ee2d5d92a86321dff32831d8cbd9245d26103887
NieYong <aofengblog@163.com>
2015-02-02 20:43:01 +0800

    for loop

---
+++ src/cn/aofeng/demo/controlflow/ForLoop.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+# iterative a string,
+for aChar in "Money":
+    print(aChar)
+print("itreative string done\n")
+
+# iterative an range
+for aInt in range(11, 19):
+    print(aInt)
+print("iterative range done\n")
+
+# iterative a list
+for elem in [1, 3, 5, 7, 9]:
+    print(elem)
+print("iterative list done ")
